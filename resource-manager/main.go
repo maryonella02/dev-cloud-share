138bca44fdf88a2b93f03a80b7aac77923ed027e
Marinela Branzeanu <marinela.branzeanu@meliora.md>
2023-04-22 17:10:21 +0300

    Create indexes for better performance

--- resource-manager/main.go
+++ resource-manager/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"dev-cloud-share/resource-manager/controllers"
 	"dev-cloud-share/resource-manager/services"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 	"net/http"
 
@@ -26,6 +27,9 @@ func main() {
 	resourceService := services.NewResourceService(db)
 	resourceController := controllers.NewResourceController(resourceService)
 
+	// Create indexes for better performance
+	createIndexes(db)
+
 	// Set up the HTTP server and routes
 	router := mux.NewRouter()
 	resourceController.RegisterRoutes(router)
@@ -34,5 +38,33 @@ func main() {
 	err = http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func createIndexes(db *mongo.Database) {
+	// Index for the 'lender_id' field in the 'resources' collection
+	resourcesCollection := db.Collection("resources")
+	lenderIDIndexModel := mongo.IndexModel{
+		Keys: bson.M{
+			"lender_id": 1, // 1 for ascending order
+		},
+		Options: options.Index().SetName("lender_id_index"),
+	}
+	_, err := resourcesCollection.Indexes().CreateOne(context.Background(), lenderIDIndexModel)
+	if err != nil {
+		log.Fatalf("Failed to create index for lender_id: %v", err)
+	}
+
+	// Index for the 'resources' field in the 'borrowers' collection
+	borrowersCollection := db.Collection("borrowers")
+	resourcesIndexModel := mongo.IndexModel{
+		Keys: bson.M{
+			"resources": 1, // 1 for ascending order
+		},
+		Options: options.Index().SetName("resources_index"),
+	}
+	_, err = borrowersCollection.Indexes().CreateOne(context.Background(), resourcesIndexModel)
+	if err != nil {
+		log.Fatalf("Failed to create index for resources: %v", err)
 	}
 }
