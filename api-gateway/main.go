d020feb655cb824998ff47008d9d3933713e8554
Marinela Branzeanu <marinela.branzeanu@meliora.md>
2023-04-24 18:07:11 +0300

    Use api-gateway as proxy to resource-manager

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"context"
-	"dev-cloud-share/resource-manager/controllers"
-	"dev-cloud-share/resource-manager/services"
+	"dev-cloud-share/api-gateway/controllers"
+	"dev-cloud-share/api-gateway/services"
 	"fmt"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
 )
 
 const (
@@ -19,33 +13,17 @@ const (
 )
 
 func main() {
-	// Initialize MongoDB
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db := client.Database("resource-manager")
-	resourceService := services.NewResourceService(db)
+	resourceService := services.NewResourceService(resourceManagerBaseURL)
 	resourceController := controllers.NewResourceController(resourceService)
 
-	// Setup the API Gateway routes
-	r := mux.NewRouter()
+	router := mux.NewRouter()
 
-	// Resource Manager routes
-	r.HandleFunc("/resources", resourceController.RegisterResource).Methods("POST")
-	r.HandleFunc("/resources", resourceController.GetResources).Methods("GET")
-	r.HandleFunc("/resources/{resource_id}", resourceController.UpdateResource).Methods("PUT")
-	r.HandleFunc("/resources/{resource_id}", resourceController.DeleteResource).Methods("DELETE")
-	r.HandleFunc("/allocations", resourceController.AllocateResource).Methods("POST")
-	r.HandleFunc("/allocations/{allocation_id}", resourceController.ReleaseResource).Methods("DELETE")
+	resourceController.RegisterRoutes(router.PathPrefix("/api/v1").Subrouter())
 
-	// TODO: Add more routes for other microservices
+	err := http.ListenAndServe("localhost:8081", router)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// Start the API Gateway server
-	fmt.Println("API Gateway listening on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", r))
 }
