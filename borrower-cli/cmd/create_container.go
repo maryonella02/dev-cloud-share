93f1e1ed267574e039b33986bce471dcb3de04a9
Marinela Branzeanu <marinela.branzeanu@meliora.md>
2023-06-03 15:40:38 +0300

    Add flags to create-container command

--- borrower-cli/cmd/create_container.go
+++ borrower-cli/cmd/create_container.go
@@ -10,28 +10,45 @@ import (
 	"os"
 )
 
-var image string
+var (
+	image    string
+	cpuCores int
+	memoryMB int
+)
 
 var createContainerCmd = &cobra.Command{
 	Use:   "create-container",
 	Short: "Create a new container",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Creating container with image:", image)
-		createContainer(image)
+		createContainer(image, cpuCores, memoryMB)
 	},
 }
 
 func init() {
 	createContainerCmd.Flags().StringVarP(&image, "image", "i", "", "Image to use for creating the container (required)")
+	createContainerCmd.Flags().IntVar(&cpuCores, "cpu-cores", 0, "CPU cores for the container")
+	createContainerCmd.Flags().IntVar(&memoryMB, "memory-mb", 0, "Memory limit in MB for the container")
 	createContainerCmd.MarkFlagRequired("image")
 	rootCmd.AddCommand(createContainerCmd)
 }
-func createContainer(image string) {
+
+func createContainer(image string, cpuCores, memoryMB int) {
+	// Convert CPU cores to nano-cpus (10^-9)
+	nanoCPUs := int64(cpuCores * 1000000000)
+
+	// Convert memory limit to bytes
+	memoryBytes := int64(memoryMB * 1024 * 1024)
+
 	// Prepare the POST request
 	url := "https://localhost:8440/api/v1/containers"
 	payload := map[string]interface{}{
-		"image": image,
+		"image":     image,
+		"nano-cpus": nanoCPUs,
+		"memory":    memoryBytes,
 	}
+	// TODO: use defined values earlier when requesting resource, if no resource constrains specified
+
 	data, _ := json.Marshal(payload)
 	body := bytes.NewReader(data)
 	req, _ := http.NewRequest("POST", url, body)
@@ -65,4 +82,8 @@ func createContainer(image string) {
 	}
 }
 
-//./borrower-cli create-container --image nginx
+// ./borrower-cli create-container --image nginx --cpu-cores 2 --memory-mb 1024
+
+// The code will convert:
+// cpu-cores value of 2 to 2000000000 nano-cpus
+// memory-mb value of 1024 to 1073741824 bytes, and send them to the gateway
